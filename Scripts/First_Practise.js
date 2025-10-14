a2e70ca5ab30a530589859c5a38f4b4dca250d22
sayyamworks <sardarsayyam027@gmail.com>
2025-10-14 22:04:45 +0500

    first commit

---
+++ Scripts/First_Practise.js
@@ -0,0 +1,7 @@
+console.log("Q jani Kesa hai???");
+console.log(2*2);
+console.log("My "+"Name "+"is "+"Sayyam...");
+console.warn("This is a warning...");
+console.error("Stop this. This is an Error...");
+// alert("my name is Sayyam");
+console.clear();
