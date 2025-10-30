ba75634312e00db8b52c7936f385272c77f98f33
sayyamworks <sardarsayyam027@gmail.com>
2025-10-30 18:37:20 +0500

    Second Practise Js

---
+++ Scripts/Second_Practise.js
@@ -0,0 +1,21 @@
+/*Variables names*/ 
+let message = 'Hi! Sayyam.';
+console.log('Hello Sayyam.');
+console.log(message);
+
+/*Variables Updates*/ 
+let num = 1;
+num = num + 2;                //3
+console.log(num);     
+num +=7;                     //10
+console.log(num);     
+num -=2;                     //8
+console.log(num);
+num *=3;                    //24
+console.log(num);
+num /=2;                    //12
+console.log(num);
+num++;                      //13
+console.log(num);
+num--;                      //12
+console.log(num);
