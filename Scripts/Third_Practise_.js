be7d602efb56707af713a077a9e5212724c5a1f8
sayyamworks <sardarsayyam027@gmail.com>
2026-02-01 15:31:20 +0500

    Third Practise Js

---
+++ Scripts/Third_Practise_.js
@@ -0,0 +1,18 @@
+let myname = 'Sayyam';
+console.log('sayyam');
+console.log(myname);
+let num = 1;
+num = num + 2;
+console.log(num);
+num += 10;
+console.log(num);
+num -= 4;
+console.log(num);
+num *= 7;
+console.log(num);
+num /= 3;
+console.log(num);
+num ++;
+console.log(num);
+num --
+console.log(num);
